913076271f88f62ab9f44f5461be22ddeef46a6a
Bharti Sharma <sharma.bharti1510@gmail.com>
2025-09-02 14:59:46 +0530

    appwrite auth service

---
+++ megaBlogAppwrite/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1>App write blog</h1>
+{
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+}
+    </>
+  )
+}
+
+export default App
