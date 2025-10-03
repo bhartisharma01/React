4167be17a8ec28970c2261fa52da4c8e66fef84f
Bharti Sharma <sharma.bharti1510@gmail.com>
2025-10-03 17:06:49 +0530

    redux, Button, Input, Select, Login, signup component

--- megaBlogAppwrite/src/App.jsx
+++ megaBlogAppwrite/src/App.jsx
@@ -1,16 +1,40 @@
 
+import { useEffect, useState } from 'react'
+import authService from './auth/auth';
 import './App.css'
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-
-  return (
-    <>
-<h1>App write blog</h1>
-{
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-}
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    }).catch((err) => {
+      console.log("error", err)
+    })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
--- megaBlogAppwrite/src/main.jsx
+++ megaBlogAppwrite/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home />,
+    //   },
+    // ]
+  }])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
